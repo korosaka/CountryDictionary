428ff3f1d9aaf6ce0f724a242d05750d3ebf3054
koro <koro.nn99@gmail.com>
2021-03-27 19:12:53 -0700

    Initial Commit

---
+++ CountryDictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountryDictionary
+//
+//  Created by Koro Saka on 2021-03-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
